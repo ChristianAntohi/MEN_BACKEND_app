149e7090f4b85508acdf640749690f06257b4307
ChristianAntohi <72394741+ChristianAntohi@users.noreply.github.com>
2023-04-23 12:01:13 +0300

    simple commit

--- routes/propertyRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const propertyController = require('../controllers/propertyController');
-const upload = require('../middleware/upload');
-
-// POST route pentru încărcarea unei proprietăți
-router.post('/properties', upload.array('images', 5), propertyController.addProperty);
-
-module.exports = router;
